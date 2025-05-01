a4cd769e229b43494318359eec486b2849ec46f0
Abhinand M.V. <139503522+abhinandmv@users.noreply.github.com>
2025-05-01 21:35:58 +0530

    Update project.py

--- project.py
+++ project.py
@@ -230,8 +230,4 @@ def Stock_info(stock_ticker, n_intervals):
         return alert, "", "", "", "", "", ""
 
 if __name__ == '__main__':
-    def open_browser():
-        webbrowser.open_new('http://127.0.0.1:8050/')
-    if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
-        Timer(1, open_browser).start()
     app.run(debug=True)
